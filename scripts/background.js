6bdbefe3c64163269c327d8a3b8a1becaf78ed26
quif <nAkT7@protonmail.com>
2023-02-19 19:42:30 +0100

    formatted code and set indent to 2

--- scripts/background.js
+++ scripts/background.js
@@ -1,69 +1,57 @@
 const update_icon = () =>
-    browser.tabs.query({currentWindow: true, active: true})
-    .then(tabs => {
-        let tab = tabs[0]; // Safe to assume there will only be one result
-        if (is_valid_page(tab.url))
-            browser.browserAction.setIcon({
-                path: "../assets/icon-64.png"
-            });
-        else
-            browser.browserAction.setIcon({
-                path: "../assets/icon-64-disabled.png"
-            });
-    }, console.error)
+  browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+    let tab = tabs[0]; // Safe to assume there will only be one result
+    if (is_valid_page(tab.url))
+      browser.browserAction.setIcon({
+        path: "../assets/icon-64.png",
+      });
+    else
+      browser.browserAction.setIcon({
+        path: "../assets/icon-64-disabled.png",
+      });
+  }, console.error);
 
-browser.tabs.onActivated.addListener(update_icon)
-browser.tabs.onUpdated.addListener(update_icon)
+browser.tabs.onActivated.addListener(update_icon);
+browser.tabs.onUpdated.addListener(update_icon);
 browser.windows.onFocusChanged.addListener(update_icon);
 
 function is_valid_page(url) {
-    return url.match(/https:\/\/.*\/ebook\/.*/)
+  return url.match(/https:\/\/.*\/ebook\/.*/);
 }
-
 
 let converting = false;
 let converting_tab = -1;
 let convert_progress = {
-    title: '',
-    from_page: -1,
-    cur_page: -1,
-    to_page: -1,
-    page_count: -1,
-    time_begin: 0,
+  title: "",
+  from_page: -1,
+  cur_page: -1,
+  to_page: -1,
+  page_count: -1,
+  time_begin: 0,
 };
 
 browser.runtime.onMessage.addListener(onMessage);
 
-
 function onMessage(message) {
-
-    if (message.type === "start_converting") {
-        converting = true;
-        convert_progress = message.convert_progress;
+  if (message.type === "start_converting") {
+    converting = true;
+    convert_progress = message.convert_progress;
+  } else if (message.type === "update_tabid") {
+    converting_tab = message.tabid;
+  } else if (message.type === "is_converting") {
+    return Promise.resolve({ converting, converting_tab });
+  } else if (message.type === "get_progress") {
+    return Promise.resolve({ convert_progress });
+  } else if (message.type === "stop_converting") {
+    converting = false;
+    converting_tab = -1;
+  } else if (message.type === "update_progress") {
+    convert_progress = message.convert_progress;
+    if (convert_progress.cur_page === convert_progress.to_page) {
+      converting = false;
+      converting_tab = -1;
     }
-    else if (message.type === "update_tabid") {
-        converting_tab = message.tabid;
-    }
-    else if (message.type === "is_converting") {
-        return Promise.resolve({converting, converting_tab});
-    }
-    else if (message.type === "get_progress") {
-        return Promise.resolve({convert_progress});
-    }
-    else if (message.type === "stop_converting") {
-        converting = false;
-        converting_tab = -1;
-    } else if (message.type === "update_progress") {
-        convert_progress = message.convert_progress;
-        if (convert_progress.cur_page === convert_progress.to_page) {
-            converting = false;
-            converting_tab = -1;
-        }
-    }
-    else if (message.type === "remove_materialize_css") {
-        browser.tabs.query({currentWindow: true, active: true})
-        .then(tabs => ()=>{}
-        );
-    }
-
+  } else if (message.type === "remove_materialize_css") {
+    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => () => {});
+  }
 }
--- scripts/d4s_detection.js
+++ scripts/d4s_detection.js
@@ -1,23 +1,29 @@
 (() => {
+  // stop program if it has already been injected
+  if (window.d4s_detection) return;
+  window.d4s_detection = true;
 
-    // stop program if it has already been injected
-    if (window.d4s_detection) return;
-    window.d4s_detection = true;
+  console.log("injected d4s detection");
 
-    console.log("injected d4s detection");
+  browser.runtime.onMessage.addListener(onMessage);
+  console.log(browser);
 
+  function onMessage(message) {
+    if (message.type === "valid") {
+      let svg_container = document.evaluate(
+        '//*[@id="jpedal"]/div[2]/object',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      ).singleNodeValue;
+      let valid_d4s_site = svg_container !== null && svg_container.type === "image/svg+xml";
 
-    browser.runtime.onMessage.addListener(onMessage);
-    console.log(browser)
-
-    function onMessage(message) {
-        if (message.type === "valid") {
-            let svg_container = document.evaluate("//*[@id=\"jpedal\"]/div[2]/object", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            let valid_d4s_site = svg_container !== null && svg_container.type === "image/svg+xml";
-
-            console.log(document.evaluate("//*[@id=\"jpedal\"]/div[2]/object", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue)
-            return Promise.resolve({injected: valid_d4s_site, title: document.title});
-        }
+      console.log(
+        document.evaluate('//*[@id="jpedal"]/div[2]/object', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+          .singleNodeValue
+      );
+      return Promise.resolve({ injected: valid_d4s_site, title: document.title });
     }
-
+  }
 })();
